0853299af46180f4403130dec62c7f9928d396ea
Sagi Shadur <saroad2@gmail.com>
2020-04-04 22:16:42 +0300

    remove fit_data.py build_from_dict

--- src/eddington_core/fit_data.py
+++ src/eddington_core/fit_data.py
@@ -9,13 +9,3 @@ class FitData:
     xerr: np.ndarray
     y: np.ndarray
     yerr: np.ndarray
-
-    @staticmethod
-    def build_from_data_dict(data_dict):
-        values = list(data_dict.values())
-        return FitData(
-            x=np.array(values[0]),
-            xerr=np.array(values[1]),
-            y=np.array(values[2]),
-            yerr=np.array(values[3]),
-        )
