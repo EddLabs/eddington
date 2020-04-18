7e9bc4aadb13a24dca9ac57006f7c796e6ecf247
Sagi Shadur <saroad2@gmail.com>
2020-04-18 13:54:05 +0300

    Add FitFunction tests

---
+++ tests/registry/test_fit_function.py
@@ -0,0 +1,96 @@
+from unittest import TestCase
+import numpy as np
+
+from eddington_core import fit_function
+from eddington_core.exceptions import FitFunctionRuntimeError
+
+
+@fit_function(n=2, save=False)
+def dummy_func1(a, x):
+    return a[0] + a[1] * x ** 2
+
+
+class TestFitFunction(TestCase):
+    decimal = 5
+
+    def test_is_generator(self):
+        self.assertFalse(
+            dummy_func1.is_generator(), msg="Fit function is not a generator"
+        )
+
+    def test_is_costumed(self):
+        self.assertFalse(
+            dummy_func1.is_costumed(),
+            msg="Fit function created with fit_function decorator is not a generator",
+        )
+
+    def test_name(self):
+        self.assertEqual(
+            "dummy_func1", dummy_func1.name, msg="Name is different than expected",
+        )
+
+    def test_signature(self):
+        self.assertEqual(
+            "dummy_func1",
+            dummy_func1.signature,
+            msg="Signature is different than expected",
+        )
+
+    def test_title_name(self):
+        self.assertEqual(
+            "Dummy Func1",
+            dummy_func1.title_name,
+            msg="Title name is different than expected",
+        )
+
+    def test_call_success(self):
+        a = np.array([1, 2])
+        x = 3
+        result = dummy_func1(a, x)
+        self.assertAlmostEqual(
+            19,
+            result,
+            places=self.decimal,
+            msg="Execution result is different than expected",
+        )
+
+    def test_assign(self):
+        a = np.array([1, 2])
+        x = 3
+        assign_func = dummy_func1.assign(a)
+        result = assign_func(x)
+        self.assertAlmostEqual(
+            19,
+            result,
+            places=self.decimal,
+            msg="Execution result is different than expected",
+        )
+
+    def test_call_failure_because_of_not_enough_parameters(self):
+        a = np.array([1])
+        x = 3
+        self.assertRaisesRegex(
+            FitFunctionRuntimeError,
+            "^input length should be 2, got 1$",
+            dummy_func1,
+            a,
+            x,
+        )
+
+    def test_call_failure_because_of_too_many_parameters(self):
+        a = np.array([1, 2, 3])
+        x = 4
+        self.assertRaisesRegex(
+            FitFunctionRuntimeError,
+            "^input length should be 2, got 3$",
+            dummy_func1,
+            a,
+            x,
+        )
+
+    def test_fit_function_representation(self):
+        self.assertEqual(
+            "FitFunction(name=dummy_func1)",
+            str(dummy_func1),
+            msg="Representation is different than expected",
+        )
