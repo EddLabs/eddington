14dcbd553ed13e7eff3c647609b399c9309011ad
Sagi Shadur <saroad2@gmail.com>
2020-04-18 12:49:56 +0300

    Add FitFunctionRegistry tests

--- tests/registry/test_fit_function_registry.py
+++ tests/registry/test_fit_function_registry.py
@@ -4,6 +4,7 @@ from eddington_core import (
     FitFunctionLoadError,
     fit_function,
     FitFunctionsRegistry,
+    fit_function_generator,
 )
 
 
@@ -14,12 +15,10 @@ class TestFitFunctionRegistryAddAndRemove(TestCase):
         self.func2 = self.dummy_function(2)
         self.func3 = self.dummy_function(3)
         self.func4 = self.dummy_function(4, save=False)
-        self.funcs = [self.func1, self.func2, self.func3]
 
-        FitFunctionsRegistry.clear()
-        FitFunctionsRegistry.add(self.func1)
-        FitFunctionsRegistry.add(self.func2)
-        FitFunctionsRegistry.add(self.func3)
+        self.generator1 = self.dummy_function_generator(1)
+
+        self.funcs = [self.func1, self.func2, self.func3, self.generator1]
 
     def tearDown(self):
         FitFunctionsRegistry.clear()
@@ -39,12 +38,57 @@ class TestFitFunctionRegistryAddAndRemove(TestCase):
 
         return dummy_func
 
+    @classmethod
+    def dummy_function_generator(cls, value, save=True):
+        @fit_function_generator(
+            parameters="p",
+            name=f"dummy_generator{value}",
+            syntax=f"syntax_dummy_generator{value}",
+        )
+        def dummy_generator(param):
+            @fit_function(
+                n=2,
+                name=f"generated_dummy_function{param}_value{value}",
+                syntax=f"syntax_generated_dummy_function{param}_value{value}",
+                save=save,
+            )
+            def dummy_func(a, x):
+                return param * value
+
+            return dummy_func
+
+        return dummy_generator
+
     def test_get_function(self):
         actual_func = FitFunctionsRegistry.get(self.func1.name)
         self.assertEqual(
             self.func1,
             actual_func,
             msg="Registry get function returns different function than expected.",
+        )
+
+    def test_get_function_generator(self):
+        actual_generator = FitFunctionsRegistry.get(self.generator1.name)
+        self.assertEqual(
+            self.generator1,
+            actual_generator,
+            msg="Registry get function returns different generator than expected.",
+        )
+
+    def test_load_function(self):
+        actual_func = FitFunctionsRegistry.load(self.func1.name)
+        self.assertEqual(
+            self.func1,
+            actual_func,
+            msg="Registry get name function returns different function than expected.",
+        )
+
+    def test_load_generator(self):
+        actual_func = FitFunctionsRegistry.load(self.generator1.name, 2)
+        self.assertEqual(
+            "generated_dummy_function2_value1",
+            actual_func.name,
+            msg="Returned function is different than expected",
         )
 
     def test_exists(self):
@@ -107,13 +151,14 @@ class TestFitFunctionRegistryAddAndRemove(TestCase):
     def test_list(self):
         syntax = FitFunctionsRegistry.list()
         expected_string = """
-+-----------------+------------------------+
-|     Function    |         Syntax         |
-+-----------------+------------------------+
-| dummy_function1 | syntax_dummy_function1 |
-| dummy_function2 | syntax_dummy_function2 |
-| dummy_function3 | syntax_dummy_function3 |
-+-----------------+------------------------+"""
++---------------------+-------------------------+
+|       Function      |          Syntax         |
++---------------------+-------------------------+
+|   dummy_function1   |  syntax_dummy_function1 |
+|   dummy_function2   |  syntax_dummy_function2 |
+|   dummy_function3   |  syntax_dummy_function3 |
+| dummy_generator1(p) | syntax_dummy_generator1 |
++---------------------+-------------------------+"""
         self.assertEqual(
             expected_string[1:],
             str(syntax),
