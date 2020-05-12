92c065c7e9f539115fcfec1ffc834c5c4509395b
Sagi Shadur <saroad2@gmail.com>
2020-05-12 23:36:56 +0300

    Fix pylint and pydocstyle errors in exceptions.py

--- src/eddington_core/exceptions.py
+++ src/eddington_core/exceptions.py
@@ -1,61 +1,65 @@
-class EddingtonException(Exception):
+# pylint: disable=missing-class-docstring
+"""Exception classes for Eddington."""
+
+
+class EddingtonException(Exception):  # noqa: D101
     pass
 
 
 # Fit Function Errors
 
 
-class InvalidGeneratorInitialization(EddingtonException):
+class InvalidGeneratorInitialization(EddingtonException):  # noqa: D101
     pass
 
 
-class FitFunctionLoadError(EddingtonException):
+class FitFunctionLoadError(EddingtonException):  # noqa: D101
     pass
 
 
-class FitFunctionRuntimeError(EddingtonException):
+class FitFunctionRuntimeError(EddingtonException):  # noqa: D101
     pass
 
 
 # Fit Data Errors
 
 
-class FitDataError(EddingtonException):
+class FitDataError(EddingtonException):  # noqa: D101
     pass
 
 
-class FitDataInvalidFile(FitDataError):
+class FitDataInvalidFile(FitDataError):  # noqa: D101
     pass
 
 
-class FitDataInvalidFileSyntax(FitDataInvalidFile):
-    def __init__(self, file_name, sheet=None):
+class FitDataInvalidFileSyntax(FitDataInvalidFile):  # noqa: D101
+    def __init__(self, file_name, sheet=None):  # noqa: D107
         sheet_msg = "" if sheet is None else f' in sheet "{sheet}"'
         msg = f'"{file_name}" has invalid syntax{sheet_msg}.'
         super(FitDataInvalidFileSyntax, self).__init__(msg)
 
 
-class FitDataColumnsLengthError(FitDataInvalidFile):
+class FitDataColumnsLengthError(FitDataInvalidFile):  # noqa: D101
     msg = "All columns in FitData should have the same length"
 
-    def __init__(self):
+    def __init__(self):  # noqa: D107
         super(FitDataColumnsLengthError, self).__init__(self.msg)
 
 
-class FitDataColumnIndexError(FitDataError):
-    def __init__(self, index, max_index):
+class FitDataColumnIndexError(FitDataError):  # noqa: D101
+    def __init__(self, index, max_index):  # noqa: D107
         super(FitDataColumnIndexError, self).__init__(
             f"No column number {index} in data. "
             f"index should be between 1 and {max_index}"
         )
 
 
-class FitDataColumnExistenceError(FitDataError):
-    def __init__(self, column):
+class FitDataColumnExistenceError(FitDataError):  # noqa: D101
+    def __init__(self, column):  # noqa: D107
         super(FitDataColumnExistenceError, self).__init__(
             f'Could not find column "{column}" in data'
         )
 
 
-class FitDataColumnsSelectionError(FitDataError):
+class FitDataColumnsSelectionError(FitDataError):  # noqa: D101
     pass
