e2c74d80246b15033d40e6896727a99cf8f65065
Sagi Shadur <saroad2@gmail.com>
2020-05-12 18:54:00 +0300

    Fix pylint and pydocstyle errors in print_util.py

--- src/eddington_core/print_util.py
+++ src/eddington_core/print_util.py
@@ -1,21 +1,35 @@
+"""Printing functions for displaying numbers with given precision."""
 import math
 
 
-def to_relevant_precision(a):
-    if a == 0:
+def to_relevant_precision(decimal):
+    """
+    Get relevant precision of a decimal number.
+
+    :param decimal: a floating point number.
+    :return: a tuple of a and n such that: decimal = a * 10^(-b).
+    """
+    if decimal == 0:
         return 0, 0
     precision = 0
-    abs_a = math.fabs(a)
+    abs_a = math.fabs(decimal)
     while abs_a < 1.0:
         abs_a *= 10
         precision += 1
-    if a < 0:
+    if decimal < 0:
         return -abs_a, precision
     return abs_a, precision
 
 
-def to_precise_string(a, n):
-    new_a, precision = to_relevant_precision(a)
-    if precision < 3:
-        return f"{a:.{n + precision}f}"
-    return f"{new_a:.{n}f}e-0{precision}"
+def to_precise_string(decimal, precision):
+    """
+    Returns a decimal as string with desired precision.
+
+    :param decimal: a floating point number.
+    :param precision: the desired precision to return the number.
+    :return: a string representing the decimal with given precision.
+    """
+    new_decimal, relevant_precision = to_relevant_precision(decimal)
+    if relevant_precision < 3:
+        return f"{decimal:.{precision + relevant_precision}f}"
+    return f"{new_decimal:.{precision}f}e-0{relevant_precision}"
