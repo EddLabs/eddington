c63953d5efb21ce65a128e99decde0d44e06ddc5
Sagi Shadur <saroad2@gmail.com>
2020-06-15 21:24:43 +0300

    make test_random_util.py use pytest

--- tests/test_random_util.py
+++ tests/test_random_util.py
@@ -1,42 +1,51 @@
-from unittest import TestCase
+import pytest
 import numpy as np
-from mock import patch
 
 from eddington_core.random_util import random_array, random_error, random_sigma
 
 
-class TestRandomUtil(TestCase):
+n = 35
+returned = np.random.uniform(size=n)
 
-    n = 35
-    returned = np.random.uniform(size=n)
 
-    @patch("numpy.random.uniform")
-    def test_random_array(self, uniform):
-        uniform.return_value = self.returned
-        min_val = 1
-        max_val = 5
-        actual = random_array(min_val=min_val, max_val=max_val, size=self.n)
-        np.testing.assert_equal(
-            actual, self.returned, err_msg="Random array returned unexpected array"
-        )
-        uniform.assert_called_once_with(min_val, max_val, size=self.n)
+@pytest.fixture
+def uniform(mocker):
+    mock = mocker.patch("numpy.random.uniform")
+    mock.return_value = returned
+    return mock
 
-    @patch("numpy.random.exponential")
-    def test_random_sigma(self, exponential):
-        exponential.return_value = self.returned
-        average_sigma = 0.5
-        actual = random_sigma(average_sigma=average_sigma, size=self.n)
-        np.testing.assert_equal(
-            actual, self.returned, err_msg="Random sigma returned unexpected array"
-        )
-        exponential.assert_called_once_with(average_sigma, size=self.n)
 
-    @patch("numpy.random.normal")
-    def test_random_error(self, normal):
-        normal.return_value = self.returned
-        scales = np.arange(0, self.n)
-        actual = random_error(scales=scales)
-        np.testing.assert_equal(
-            actual, self.returned, err_msg="Random error returned unexpected array"
-        )
-        normal.assert_called_once_with(scale=scales)
+@pytest.fixture
+def exponential(mocker):
+    mock = mocker.patch("numpy.random.exponential")
+    mock.return_value = returned
+    return mock
+
+
+@pytest.fixture
+def normal(mocker):
+    mock = mocker.patch("numpy.random.normal")
+    mock.return_value = returned
+    return mock
+
+
+def test_random_array(uniform):
+    min_val = 1
+    max_val = 5
+    actual = random_array(min_val=min_val, max_val=max_val, size=n)
+    assert actual == pytest.approx(returned), "Random array returned unexpected array"
+    uniform.assert_called_once_with(min_val, max_val, size=n)
+
+
+def test_random_sigma(exponential):
+    average_sigma = 0.5
+    actual = random_sigma(average_sigma=average_sigma, size=n)
+    assert actual == pytest.approx(returned), "Random sigma returned unexpected array"
+    exponential.assert_called_once_with(average_sigma, size=n)
+
+
+def test_random_error(normal):
+    scales = np.arange(0, n)
+    actual = random_error(scales=scales)
+    assert actual == pytest.approx(returned), "Random error returned unexpected array"
+    normal.assert_called_once_with(scale=scales)
