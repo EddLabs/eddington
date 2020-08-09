cbac20946a949facff1d058022b149148875cf81
Sagi Shadur <saroad2@gmail.com>
2020-08-09 14:11:16 +0300

    Fix static code analysis errors

--- src/eddington/fit_functions_list.py
+++ src/eddington/fit_functions_list.py
@@ -227,13 +227,14 @@ def sin(a, x):
 @fit_function(
     n=4,
     syntax="a[0] * exp( - ((x - a[1]) / a[2]) ** 2) + a[3]",
-    x_derivative=lambda a, x: a[0] * np.exp(- ((x - a[1])\
-         / a[2]) ** 2) * (-2 * (x - a[1]) / a[2]),
+    x_derivative=lambda a, x: a[0]
+    * np.exp(-(((x - a[1]) / a[2]) ** 2))  # noqa: W503
+    * (-2 * (x - a[1]) / a[2]),  # noqa: W503
     a_derivative=lambda a, x: np.stack(
         [
-            np.exp(- ((x - a[1]) / a[2]) ** 2),
-            a[0] * np.exp(- ((x - a[1]) / a[2]) ** 2) * (2 * (x - a[1]) / a[2]),
-            a[0] * np.exp(- ((x - a[1]) / a[2]) ** 2) * (2 * (x - a[1]) / (a[2] ** 2)),
+            np.exp(-(((x - a[1]) / a[2]) ** 2)),
+            a[0] * np.exp(-(((x - a[1]) / a[2]) ** 2)) * (2 * (x - a[1]) / a[2]),
+            a[0] * np.exp(-(((x - a[1]) / a[2]) ** 2)) * (2 * (x - a[1]) / (a[2] ** 2)),
             np.ones(shape=x.shape),
         ]
     ),
@@ -246,4 +247,4 @@ def normal(a, x):
     :param x: free parameter
     :return: float
     """
-    return a[0] * np.exp(- ((x - a[1]) / a[2]) **2) + a[3]
+    return a[0] * np.exp(-(((x - a[1]) / a[2]) ** 2)) + a[3]
