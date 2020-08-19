620e52d2e95b74088170772633423414e57ac230
Sagi Shadur <saroad2@gmail.com>
2020-08-19 23:16:16 +0300

    Add test in test_read_fit_data_from_file.py for missing case

--- tests/fit_data/test_read_fit_data_from_file.py
+++ tests/fit_data/test_read_fit_data_from_file.py
@@ -10,9 +10,9 @@ from eddington import FitData, FitDataInvalidFileSyntax
 from tests.fit_data import COLUMNS, CONTENT, ROWS, VALUES
 
 DummyCell = namedtuple("DummyCell", "value")
-file_name = "file"
-filepath = Path("path/to") / file_name
-sheet_name = "sheet"
+FILENAME = "file"
+FILE_PATH = Path("path/to") / FILENAME
+SHEET_NAME = "sheet"
 
 
 def check_data_by_keys(actual_fit_data):
@@ -63,9 +63,9 @@ def read_csv(mocker):
     reader = mocker.patch("csv.reader")
     m_open = mock_open()
 
-    def actual_read(**kwargs):
+    def actual_read(file_path, **kwargs):
         with patch("eddington.fit_data.open", m_open):
-            actual_fit_data = FitData.read_from_csv(filepath, **kwargs)
+            actual_fit_data = FitData.read_from_csv(file_path, **kwargs)
         return actual_fit_data
 
     return actual_read, dict(reader=reader, row_setter=set_csv_rows)
@@ -92,8 +92,8 @@ def mock_open_workbook(mocker):
 
 @pytest.fixture
 def read_excel(mock_open_workbook):
-    def actual_read(**kwargs):
-        return FitData.read_from_excel(filepath, sheet_name, **kwargs)
+    def actual_read(file_path, **kwargs):
+        return FitData.read_from_excel(file_path, SHEET_NAME, **kwargs)
 
     return actual_read, dict(reader=mock_open_workbook, row_setter=set_excel_rows)
 
@@ -102,7 +102,7 @@ def read_excel(mock_open_workbook):
 def test_read_with_headers_successful(read, mocks):
     mocks["row_setter"](mocks["reader"], ROWS)
 
-    actual_fit_data = read()
+    actual_fit_data = read(FILE_PATH)
 
     check_data_by_keys(actual_fit_data)
     check_columns(actual_fit_data)
@@ -112,7 +112,7 @@ def test_read_with_headers_successful(read, mocks):
 def test_read_without_headers_successful(read, mocks):
     mocks["row_setter"](mocks["reader"], CONTENT)
 
-    actual_fit_data = read()
+    actual_fit_data = read(FILE_PATH)
 
     check_data_by_indexes(actual_fit_data)
     check_columns(actual_fit_data)
@@ -125,14 +125,14 @@ def test_read_without_headers_unsuccessful(read, mocks):
     mocks["row_setter"](mocks["reader"], rows)
 
     with pytest.raises(FitDataInvalidFileSyntax):
-        read()
+        read(FILE_PATH)
 
 
 @parametrize_plus("read, mocks", [fixture_ref(read_csv), fixture_ref(read_excel)])
 def test_read_with_x_column(read, mocks):
     mocks["row_setter"](mocks["reader"], ROWS)
 
-    actual_fit_data = read(x_column=3)
+    actual_fit_data = read(FILE_PATH, x_column=3)
 
     check_columns(actual_fit_data, x_column=2, xerr_column=3, y_column=4, yerr_column=5)
 
@@ -141,7 +141,7 @@ def test_read_with_x_column(read, mocks):
 def test_read_with_xerr_column(read, mocks):
     mocks["row_setter"](mocks["reader"], ROWS)
 
-    actual_fit_data = read(xerr_column=3)
+    actual_fit_data = read(FILE_PATH, xerr_column=3)
 
     check_columns(actual_fit_data, x_column=0, xerr_column=2, y_column=3, yerr_column=4)
 
@@ -150,7 +150,7 @@ def test_read_with_xerr_column(read, mocks):
 def test_read_with_y_column(read, mocks):
     mocks["row_setter"](mocks["reader"], ROWS)
 
-    actual_fit_data = read(y_column=5)
+    actual_fit_data = read(FILE_PATH, y_column=5)
 
     check_columns(actual_fit_data, x_column=0, xerr_column=1, y_column=4, yerr_column=5)
 
@@ -159,6 +159,16 @@ def test_read_with_y_column(read, mocks):
 def test_read_with_yerr_column(read, mocks):
     mocks["row_setter"](mocks["reader"], ROWS)
 
-    actual_fit_data = read(yerr_column=5)
+    actual_fit_data = read(FILE_PATH, yerr_column=5)
 
     check_columns(actual_fit_data, x_column=0, xerr_column=1, y_column=2, yerr_column=4)
+
+
+@parametrize_plus("read, mocks", [fixture_ref(read_csv), fixture_ref(read_excel)])
+def test_read_string_path_successful(read, mocks):
+    mocks["row_setter"](mocks["reader"], ROWS)
+
+    actual_fit_data = read(str(FILE_PATH))
+
+    check_data_by_keys(actual_fit_data)
+    check_columns(actual_fit_data)
