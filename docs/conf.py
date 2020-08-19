aaf496158d4f6c5f8c0bf8411a352819f63fca67
Sagi Shadur <saroad2@gmail.com>
2020-08-19 16:37:16 +0300

    Uncomment unnecessary line in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -131,7 +131,9 @@ html_logo = "images/eddington.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+
+# UNCOMMENT THIS WHEN NEEDED
+# html_static_path = ["_static"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
