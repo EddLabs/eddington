8953dea7911e949a61ff0f89bf5784f6e22441bc
Sagi Shadur <saroad2@gmail.com>
2020-09-07 23:35:35 +0300

    Update versions of libraries

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ version = "0.0.15"
 on_rtd = os.environ.get(READTHEDOCS) == "True"
 install_requires = []
 if not on_rtd:
-    install_requires = ["scipy >= 1.5.2", "numpy >= 1.19.1", "openpyxl >= 3.0.4"]
+    install_requires = ["scipy >= 1.5.2", "numpy >= 1.19.1", "openpyxl >= 3.0.5"]
 setup(version=version, install_requires=install_requires)
