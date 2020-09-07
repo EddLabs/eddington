e86adb9fb00867e46fc8c941e259e3d7464ec1c8
Sagi Shadur <saroad2@gmail.com>
2020-09-07 23:38:33 +0300

    Add matplotlib to dependencies

--- setup.py
+++ setup.py
@@ -9,5 +9,10 @@ version = "0.0.15"
 on_rtd = os.environ.get(READTHEDOCS) == "True"
 install_requires = []
 if not on_rtd:
-    install_requires = ["scipy >= 1.5.2", "numpy >= 1.19.1", "openpyxl >= 3.0.5"]
+    install_requires = [
+        "scipy >= 1.5.2",
+        "numpy >= 1.19.1",
+        "openpyxl >= 3.0.5",
+        "matplotlib >= 3.3.1",
+    ]
 setup(version=version, install_requires=install_requires)
