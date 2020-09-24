35e8902739cc20ad92b1dfd59b05acbc09d62684
Sagi Shadur <saroad2@gmail.com>
2020-09-24 09:55:00 +0300

    Refactor plotting methods

--- tests/plot/conftest.py
+++ tests/plot/conftest.py
@@ -1,6 +1,12 @@
 from pytest_cases import fixture
+import matplotlib.pyplot as plt
 
 
 @fixture
-def mock_plt(mocker):
-    return mocker.patch("eddington.plot.plt")
+def mock_figure(mocker):
+    return mocker.patch.object(plt, "figure").return_value
+
+
+@fixture
+def mock_plt_show(mocker):
+    return mocker.patch.object(plt, "show")
--- tests/plot/test_plot_fitting.py
+++ tests/plot/test_plot_fitting.py
@@ -24,133 +24,128 @@ TITLE_NAME = "Title"
 
 
 @case(tags=[DOES_NOT_HAVE_LEGEND])
-def case_no_args(mock_plt):
+def case_no_args(mock_figure):
     x = np.arange(0.1, 10.9, step=0.0108)
 
     kwargs = dict(a=A1)
-    plot_calls = [
-        ([x, FUNC(A1, x)], dict(figure=mock_plt.figure.return_value, label=A1_REPR))
-    ]
-    return kwargs, plot_calls, mock_plt
+    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR))]
+    return kwargs, plot_calls, mock_figure
 
 
 @case(tags=[DOES_NOT_HAVE_LEGEND])
-def case_xmin(mock_plt):
+def case_xmin(mock_figure):
     x = np.arange(-10, 10.9, step=0.0209)
 
     kwargs = dict(a=A1, xmin=-10)
-    plot_calls = [
-        ([x, FUNC(A1, x)], dict(figure=mock_plt.figure.return_value, label=A1_REPR))
-    ]
-    return kwargs, plot_calls, mock_plt
+    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR))]
+    return kwargs, plot_calls, mock_figure
 
 
 @case(tags=[DOES_NOT_HAVE_LEGEND])
-def case_xmax(mock_plt):
+def case_xmax(mock_figure):
     x = np.arange(0.1, 20, step=0.0199)
 
     kwargs = dict(a=A1, xmax=20)
-    plot_calls = [
-        ([x, FUNC(A1, x)], dict(figure=mock_plt.figure.return_value, label=A1_REPR))
-    ]
-    return kwargs, plot_calls, mock_plt
+    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR))]
+    return kwargs, plot_calls, mock_figure
 
 
 @case(tags=[DOES_NOT_HAVE_LEGEND])
-def case_step(mock_plt):
+def case_step(mock_figure):
     x = np.arange(0.1, 10.9, step=0.1)
 
     kwargs = dict(a=A1, step=0.1)
-    plot_calls = [
-        ([x, FUNC(A1, x)], dict(figure=mock_plt.figure.return_value, label=A1_REPR))
-    ]
-    return kwargs, plot_calls, mock_plt
+    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR))]
+    return kwargs, plot_calls, mock_figure
 
 
 @case(tags=[HAS_LEGEND])
-def case_a_list_with_legend(mock_plt):
+def case_a_list_with_legend(mock_figure):
     x = np.arange(0.1, 10.9, step=0.0108)
 
     kwargs = dict(a=[A1, A2])
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(figure=mock_plt.figure.return_value, label=A1_REPR)),
-        ([x, FUNC(A2, x)], dict(figure=mock_plt.figure.return_value, label=A2_REPR)),
+        ([x, FUNC(A1, x)], dict(label=A1_REPR)),
+        ([x, FUNC(A2, x)], dict(label=A2_REPR)),
     ]
-    return kwargs, plot_calls, mock_plt
+    return kwargs, plot_calls, mock_figure
 
 
 @case(tags=[DOES_NOT_HAVE_LEGEND])
-def case_a_list_without_legend(mock_plt):
+def case_a_list_without_legend(mock_figure):
     x = np.arange(0.1, 10.9, step=0.0108)
 
     kwargs = dict(a=[A1, A2], legend=False)
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(figure=mock_plt.figure.return_value, label=A1_REPR)),
-        ([x, FUNC(A2, x)], dict(figure=mock_plt.figure.return_value, label=A2_REPR)),
+        ([x, FUNC(A1, x)], dict(label=A1_REPR)),
+        ([x, FUNC(A2, x)], dict(label=A2_REPR)),
     ]
-    return kwargs, plot_calls, mock_plt
+    return kwargs, plot_calls, mock_figure
 
 
 @case(tags=[HAS_LEGEND])
-def case_a_dict_with_legend(mock_plt):
+def case_a_dict_with_legend(mock_figure):
     x = np.arange(0.1, 10.9, step=0.0108)
     one = "one"
     two = "two"
     kwargs = dict(a={one: A1, two: A2})
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(figure=mock_plt.figure.return_value, label=one)),
-        ([x, FUNC(A2, x)], dict(figure=mock_plt.figure.return_value, label=two)),
+        ([x, FUNC(A1, x)], dict(label=one)),
+        ([x, FUNC(A2, x)], dict(label=two)),
     ]
-    return kwargs, plot_calls, mock_plt
+    return kwargs, plot_calls, mock_figure
 
 
 @case(tags=[DOES_NOT_HAVE_LEGEND])
-def case_a_dict_without_legend(mock_plt):
+def case_a_dict_without_legend(mock_figure):
     x = np.arange(0.1, 10.9, step=0.0108)
     one = "one"
     two = "two"
     kwargs = dict(a={one: A1, two: A2}, legend=False)
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(figure=mock_plt.figure.return_value, label=one)),
-        ([x, FUNC(A2, x)], dict(figure=mock_plt.figure.return_value, label=two)),
+        ([x, FUNC(A1, x)], dict(label=one)),
+        ([x, FUNC(A2, x)], dict(label=two)),
     ]
-    return kwargs, plot_calls, mock_plt
+    return kwargs, plot_calls, mock_figure
 
 
 @case(tags=[DOES_NOT_HAVE_LEGEND])
-def case_a_redundent_precision(mock_plt):
+def case_a_redundent_precision(mock_figure):
     x = np.arange(0.1, 10.9, step=0.0108)
 
     kwargs = dict(a=A3)
-    plot_calls = [
-        ([x, FUNC(A3, x)], dict(figure=mock_plt.figure.return_value, label=A3_REPR))
-    ]
-    return kwargs, plot_calls, mock_plt
+    plot_calls = [([x, FUNC(A3, x)], dict(label=A3_REPR))]
+    return kwargs, plot_calls, mock_figure
 
 
-@parametrize_with_cases(argnames="kwargs, plot_calls, plt", cases=THIS_MODULE)
-def test_plot_fitting_without_boundaries(kwargs, plot_calls, plt):
+@parametrize_with_cases(argnames=["kwargs", "plot_calls", "figure"], cases=THIS_MODULE)
+def test_plot_fitting_without_boundaries(kwargs, plot_calls, figure):
     plot_fitting(data=FIT_DATA, func=FUNC, title_name=TITLE_NAME, **kwargs)
-    assert_calls(plt.plot, plot_calls, rel=EPSILON)
+    ax = figure.add_subplot.return_value
+    assert_calls(ax.plot, plot_calls, rel=EPSILON)
 
 
 @parametrize_with_cases(
-    argnames="kwargs, plot_calls, plt", cases=THIS_MODULE, has_tag=HAS_LEGEND
+    argnames=["kwargs", "plot_calls", "figure"], cases=THIS_MODULE, has_tag=HAS_LEGEND
 )
-def test_legend_was_called(kwargs, plot_calls, plt):
+def test_legend_was_called(kwargs, plot_calls, figure):
     plot_fitting(data=FIT_DATA, func=FUNC, title_name=TITLE_NAME, **kwargs)
-    plt.legend.assert_called_once()
+    ax = figure.add_subplot.return_value
+    ax.legend.assert_called_once()
 
 
 @parametrize_with_cases(
-    argnames="kwargs, plot_calls, plt", cases=THIS_MODULE, has_tag=DOES_NOT_HAVE_LEGEND
+    argnames=["kwargs", "plot_calls", "figure"],
+    cases=THIS_MODULE,
+    has_tag=DOES_NOT_HAVE_LEGEND,
 )
-def test_legend_was_not_called(kwargs, plot_calls, plt):
+def test_legend_was_not_called(kwargs, plot_calls, figure):
     plot_fitting(data=FIT_DATA, func=FUNC, a=A1, title_name=TITLE_NAME, legend=False)
-    plt.legend.assert_not_called()
+    ax = figure.add_subplot.return_value
+    ax.legend.assert_not_called()
 
 
-def test_plot_unknown_a_type(mock_plt):
+def test_plot_unknown_a_type(mock_figure):
     with pytest.raises(
         PlottingError,
         match=(
