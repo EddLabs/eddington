76c32cd93fc0f045595145a0f74a99ba871c6d4c
Sagi Shadur <saroad2@gmail.com>
2020-09-28 12:23:11 +0300

    Update setup files

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 import os
-import sys
 
 from setuptools import setup
 
-READTHEDOCS = "READTHEDOCS"
 
 version = "0.0.17"
-on_rtd = os.environ.get(READTHEDOCS) == "True"
-install_requires = ["click >= 7.1.2", "ptable >= 0.7.2"]
-if [sys.version_info.major, sys.version_info.minor] == [3, 6]:
-    install_requires += ["dataclasses >= 0.7"]
-if not on_rtd:
-    install_requires.extend(
-        [
-            "scipy >= 1.5.2",
-            "numpy >= 1.19.2",
-            "openpyxl >= 3.0.5",
-            "matplotlib >= 3.3.2",
-        ]
-    )
+install_requires = [
+    "click >= 7.1.2",
+    "ptable >= 0.7.2",
+    'dataclasses >= 0.7; python_version == "3.6"',
+    "scipy >= 1.5.2",
+    "numpy >= 1.19.2",
+    "openpyxl >= 3.0.5",
+    "matplotlib >= 3.3.2",
+]
+
+if os.environ.get("READTHEDOCS") == "True":
+    install_requires = install_requires[:3]
+
 setup(version=version, install_requires=install_requires)
