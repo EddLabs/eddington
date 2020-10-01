883428f687ae8349f6e2627c49c832042449d5da
sonicrs <46223565+sonicrs@users.noreply.github.com>
2020-10-01 18:41:28 +0300

    Update true_stats.py
    added finite difference option

--- src/eddington/true_stats.py
+++ src/eddington/true_stats.py
@@ -6,7 +6,7 @@ from scipy.linalg import inv
 
 
 def chi2(params, *args):
-    #function, x, y, x_variance, y_variance, func_derivative_by_x
+    # function, x, y, x_variance, y_variance, func_derivative_by_x
     func, x, y, xvar, yvar, derr = args
 
     return (y - func(params, x))/np.sqrt(xvar*derr(params, x) + yvar)
@@ -24,18 +24,24 @@ class ModifiedODR():
     def run(self):
         data  = self.data
         model = self.model
+        if model.jacd is None:
+            # use a simple 2-point finnite difference scheme
+            # TODO: add support for controllable FD stepsize, as in ODR
+            xderr = lambda params, x:(model.fcn(params, x + 1e-8) - model.fcn(params, x - 1e-8))/(2e-8)
+        else:
+            xderr = model.jacd
         
         #fit. FOR NOW, NUMERICALLY DERIVATES CHI2 (REQUIRES f_xa FOR ANALYTICAL)
         res = least_squares(chi2, self.beta0, '3-point', method='trf',
                             args=(model.fcn,
                                   data.x, data.y,
                                   np.square(data.sx), np.square(data.sy),
-                                  model.jacd))
+                                  xderr))
 
-        #calculate approximate covariance matrix using Gauss-Newton approximation
+        # calculate approximate covariance matrix using Gauss-Newton approximation
         cov_mat = inv(res.jac.T@res.jac)
 
-        #make "output" object
+        # make "output" object
         output = object()
         output.beta = res.x
         output.chi2 = res.cost
