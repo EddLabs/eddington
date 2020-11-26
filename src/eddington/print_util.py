3e4b0eb9957d7e49e0b4b8d28c9cddb0a2d0ee6a
Sagi Shadur <saroad2@gmail.com>
2020-11-26 14:21:17 +0200

    Fix statue errors

--- src/eddington/print_util.py
+++ src/eddington/print_util.py
@@ -9,6 +9,19 @@ def to_relevant_precision_string(
     relevant_digits: int = DEFAULT_PRECISION,
     max_string_length: int = DEFAULT_MAX_STRING_LENGTH,
 ) -> str:
+    """
+    Convert a decimal into string while preserving relevant digits.
+
+    :param decimal: The float to convert to string.
+    :type decimal: float
+    :param relevant_digits: number of relevant digits to preserve while converting to
+        string.
+    :type relevant_digits: int
+    :param max_string_length: maximum string length when decimal is not normalized.
+    :type max_string_length: int
+    :returns: decimal as string
+    :rtype: str
+    """
     if decimal in [np.inf, np.nan, -np.inf]:
         return str(decimal)
     order = order_of_magnitude(decimal)
@@ -19,6 +32,18 @@ def to_relevant_precision_string(
 def to_digit_string(
     decimal: float, digit: int, max_string_length: int = DEFAULT_MAX_STRING_LENGTH
 ) -> str:
+    """
+    Convert a decimal into string while preserving given digit.
+
+    :param decimal: The float to convert to string.
+    :type decimal: float
+    :param digit: The lowest digit to preserve while converting to string.
+    :type digit: int
+    :param max_string_length: maximum string length when decimal is not normalized.
+    :type max_string_length: int
+    :returns: decimal as string
+    :rtype: str
+    """
     if decimal == 0 and digit < 0:
         return f"{0:.{-digit}f}"
     if decimal in [np.inf, np.nan, -np.inf]:
@@ -28,10 +53,12 @@ def to_digit_string(
     if number_of_digits < max_string_length and -DEFAULT_MAX_STRING_LENGTH < digit < 0:
         return f"{decimal:.{-digit}f}"
     order_sign = "+" if order > 0 else "-"
-    return f"{(decimal * np.power(10.0, -order)):.{number_of_digits}f}e{order_sign}{np.abs(order)}"
+    normalized_value = decimal * np.power(10.0, -order)
+    return f"{normalized_value:.{number_of_digits}f}e{order_sign}{np.abs(order)}"
 
 
 def order_of_magnitude(decimal: float) -> int:
+    """Get the order of magnitude of the given number."""
     if decimal in [0, np.inf, np.nan, -np.inf]:
         return 0
     return int(np.floor(np.log10(np.abs(decimal))))
