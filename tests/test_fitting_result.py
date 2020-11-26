5380e8c1150bbe0d57582c9eb417439d0740fcb4
Sagi Shadur <saroad2@gmail.com>
2020-11-26 14:05:30 +0200

    Fix failed tests

--- tests/test_fitting_result.py
+++ tests/test_fitting_result.py
@@ -26,15 +26,15 @@ def case_standard():
 Initial parameters' values:
 \t1.0 3.0
 Fitted parameters' values:
-\ta[0] = 1.100 \u00B1 0.100 (9.091% error)
-\ta[1] = 2.980 \u00B1 0.760 (25.503% error)
+\ta[0] = 1.100 \u00B1 0.1000 (9.091% error)
+\ta[1] = 2.980 \u00B1 0.7600 (25.50% error)
 Fitted parameters covariance:
 [[0.01  2.3  ]
  [2.3   0.988]]
 Chi squared: 8.276
 Degrees of freedom: 5
 Chi squared reduced: 1.655
-P-probability: 0.142
+P-probability: 0.1417
 """
     fitting_result = FittingResult(**kwargs)
     return (
@@ -76,7 +76,7 @@ Fitted parameters covariance:
 Chi squared: 8.276
 Degrees of freedom: 5
 Chi squared reduced: 1.655
-P-probability: 0.142
+P-probability: 0.1417
 """
     fitting_result = FittingResult(**kwargs)
     return (
@@ -111,15 +111,15 @@ def case_with_zero_value():
 Initial parameters' values:
 \t1.0 3.0
 Fitted parameters' values:
-\ta[0] = 0.000 \u00B1 0.100 (inf% error)
-\ta[1] = 0.000 \u00B1 0.760 (inf% error)
+\ta[0] = 0.000 \u00B1 0.1000 (inf% error)
+\ta[1] = 0.000 \u00B1 0.7600 (inf% error)
 Fitted parameters covariance:
 [[0.01  2.3  ]
  [2.3   0.988]]
 Chi squared: 8.276
 Degrees of freedom: 5
 Chi squared reduced: 1.655
-P-probability: 0.142
+P-probability: 0.1417
 """
     fitting_result = FittingResult(**kwargs)
     return (
@@ -154,12 +154,12 @@ def case_with_small_p_probability():
 Initial parameters' values:
 \t1.0 3.0
 Fitted parameters' values:
-\ta[0] = 1.100 \u00B1 0.100 (9.091% error)
-\ta[1] = 2.980 \u00B1 0.760 (25.503% error)
+\ta[0] = 1.100 \u00B1 0.1000 (9.091% error)
+\ta[1] = 2.980 \u00B1 0.7600 (25.50% error)
 Fitted parameters covariance:
 [[0.01  2.3  ]
  [2.3   0.988]]
-Chi squared: 43.726
+Chi squared: 43.73
 Degrees of freedom: 5
 Chi squared reduced: 8.745
 P-probability: 2.633e-8
@@ -251,7 +251,7 @@ def test_representation(expected, fitting_result):
     actual_repr = str(fitting_result).split("\n")
     for i, (expected_line, actual_line) in enumerate(zip(expected_repr, actual_repr)):
         assert (
-            expected_line == actual_line
+            actual_line == expected_line
         ), f"Representation is different than expected on line {i}"
 
 
