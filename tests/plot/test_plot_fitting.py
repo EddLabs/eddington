82d870c93b893c876474fbaa6c8c18268aebcf56
Sagi Shadur <saroad2@gmail.com>
2021-02-07 22:33:13 +0200

    Add linestyle as plot option

--- tests/plot/test_plot_fitting.py
+++ tests/plot/test_plot_fitting.py
@@ -4,6 +4,7 @@ from pytest_cases import THIS_MODULE, case, parametrize_with_cases
 
 from eddington import FittingData, linear, plot_fitting
 from eddington.exceptions import PlottingError
+from eddington.plot import LineStyle
 from tests.util import assert_calls
 
 HAS_LEGEND = "has_legend"
@@ -28,7 +29,9 @@ def case_no_args(mock_figure):
     x = np.arange(0.1, 10.9, step=0.0108)
 
     kwargs = dict(a=A1)
-    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR, color=None))]
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="solid"))
+    ]
     return kwargs, plot_calls, mock_figure
 
 
@@ -37,7 +40,9 @@ def case_xmin(mock_figure):
     x = np.arange(-10, 10.9, step=0.0209)
 
     kwargs = dict(a=A1, xmin=-10)
-    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR, color=None))]
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="solid"))
+    ]
     return kwargs, plot_calls, mock_figure
 
 
@@ -46,7 +51,9 @@ def case_xmax(mock_figure):
     x = np.arange(0.1, 20, step=0.0199)
 
     kwargs = dict(a=A1, xmax=20)
-    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR, color=None))]
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="solid"))
+    ]
     return kwargs, plot_calls, mock_figure
 
 
@@ -55,7 +62,9 @@ def case_step(mock_figure):
     x = np.arange(0.1, 10.9, step=0.1)
 
     kwargs = dict(a=A1, step=0.1)
-    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR, color=None))]
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="solid"))
+    ]
     return kwargs, plot_calls, mock_figure
 
 
@@ -65,8 +74,8 @@ def case_a_list_with_legend(mock_figure):
 
     kwargs = dict(a=[A1, A2])
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None)),
-        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=None)),
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="solid")),
+        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=None, linestyle="solid")),
     ]
     return kwargs, plot_calls, mock_figure
 
@@ -77,8 +86,8 @@ def case_a_list_without_legend(mock_figure):
 
     kwargs = dict(a=[A1, A2], legend=False)
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None)),
-        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=None)),
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="solid")),
+        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=None, linestyle="solid")),
     ]
     return kwargs, plot_calls, mock_figure
 
@@ -90,8 +99,8 @@ def case_a_dict_with_legend(mock_figure):
     two = "two"
     kwargs = dict(a={one: A1, two: A2})
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(label=one, color=None)),
-        ([x, FUNC(A2, x)], dict(label=two, color=None)),
+        ([x, FUNC(A1, x)], dict(label=one, color=None, linestyle="solid")),
+        ([x, FUNC(A2, x)], dict(label=two, color=None, linestyle="solid")),
     ]
     return kwargs, plot_calls, mock_figure
 
@@ -103,8 +112,8 @@ def case_a_dict_without_legend(mock_figure):
     two = "two"
     kwargs = dict(a={one: A1, two: A2}, legend=False)
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(label=one, color=None)),
-        ([x, FUNC(A2, x)], dict(label=two, color=None)),
+        ([x, FUNC(A1, x)], dict(label=one, color=None, linestyle="solid")),
+        ([x, FUNC(A2, x)], dict(label=two, color=None, linestyle="solid")),
     ]
     return kwargs, plot_calls, mock_figure
 
@@ -114,7 +123,9 @@ def case_a_redundent_precision(mock_figure):
     x = np.arange(0.1, 10.9, step=0.0108)
 
     kwargs = dict(a=A3)
-    plot_calls = [([x, FUNC(A3, x)], dict(label=A3_REPR, color=None))]
+    plot_calls = [
+        ([x, FUNC(A3, x)], dict(label=A3_REPR, color=None, linestyle="solid"))
+    ]
     return kwargs, plot_calls, mock_figure
 
 
@@ -124,7 +135,9 @@ def case_string_color(mock_figure):
     color = "yellow"
 
     kwargs = dict(a=A1, color=color)
-    plot_calls = [([x, FUNC(A1, x)], dict(label=A1_REPR, color=color))]
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=color, linestyle="solid"))
+    ]
     return kwargs, plot_calls, mock_figure
 
 
@@ -135,8 +148,8 @@ def case_a_list_with_colors_list(mock_figure):
 
     kwargs = dict(a=[A1, A2], legend=False, color=[color1, color2])
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=color1)),
-        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=color2)),
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=color1, linestyle="solid")),
+        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=color2, linestyle="solid")),
     ]
     return kwargs, plot_calls, mock_figure
 
@@ -148,9 +161,9 @@ def case_a_list_with_colors_list_shorter(mock_figure):
 
     kwargs = dict(a=[A1, A2, A3], legend=False, color=[color1, color2])
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=color1)),
-        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=color2)),
-        ([x, FUNC(A3, x)], dict(label=A3_REPR, color=None)),
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=color1, linestyle="solid")),
+        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=color2, linestyle="solid")),
+        ([x, FUNC(A3, x)], dict(label=A3_REPR, color=None, linestyle="solid")),
     ]
     return kwargs, plot_calls, mock_figure
 
@@ -162,14 +175,68 @@ def case_a_dict_with_color_dict(mock_figure):
     color1, color2 = "green", "yellow"
     kwargs = dict(a={label1: A1, label2: A2}, color={label1: color1, label2: color2})
     plot_calls = [
-        ([x, FUNC(A1, x)], dict(label=label1, color=color1)),
-        ([x, FUNC(A2, x)], dict(label=label2, color=color2)),
+        ([x, FUNC(A1, x)], dict(label=label1, color=color1, linestyle="solid")),
+        ([x, FUNC(A2, x)], dict(label=label2, color=color2, linestyle="solid")),
+    ]
+    return kwargs, plot_calls, mock_figure
+
+
+@case(tags=[DOES_NOT_HAVE_LEGEND])
+def case_enum_linestyle(mock_figure):
+    x = np.arange(0.1, 10.9, step=0.0108)
+    linestyle = LineStyle.DOTTED
+
+    kwargs = dict(a=A1, linestyle=linestyle)
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="dotted"))
+    ]
+    return kwargs, plot_calls, mock_figure
+
+
+@case(tags=[DOES_NOT_HAVE_LEGEND])
+def case_a_list_with_linestyles_list(mock_figure):
+    x = np.arange(0.1, 10.9, step=0.0108)
+    linestyle1, linestyle2 = LineStyle.DOTTED, LineStyle.DASHED
+
+    kwargs = dict(a=[A1, A2], legend=False, linestyle=[linestyle1, linestyle2])
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="dotted")),
+        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=None, linestyle="dashed")),
+    ]
+    return kwargs, plot_calls, mock_figure
+
+
+@case(tags=[DOES_NOT_HAVE_LEGEND])
+def case_a_list_with_linestyles_list_shorter(mock_figure):
+    x = np.arange(0.1, 10.9, step=0.0108)
+    linestyle1, linestyle2 = LineStyle.DOTTED, LineStyle.DASHED
+
+    kwargs = dict(a=[A1, A2, A3], legend=False, linestyle=[linestyle1, linestyle2])
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=A1_REPR, color=None, linestyle="dotted")),
+        ([x, FUNC(A2, x)], dict(label=A2_REPR, color=None, linestyle="dashed")),
+        ([x, FUNC(A3, x)], dict(label=A3_REPR, color=None, linestyle="solid")),
+    ]
+    return kwargs, plot_calls, mock_figure
+
+
+@case(tags=[DOES_NOT_HAVE_LEGEND])
+def case_a_dict_with_linestyle_dict(mock_figure):
+    x = np.arange(0.1, 10.9, step=0.0108)
+    label1, label2 = "label1", "label2"
+    linestyle1, linestyle2 = LineStyle.DOTTED, LineStyle.DASHED
+    kwargs = dict(
+        a={label1: A1, label2: A2}, linestyle={label1: linestyle1, label2: linestyle2}
+    )
+    plot_calls = [
+        ([x, FUNC(A1, x)], dict(label=label1, color=None, linestyle="dotted")),
+        ([x, FUNC(A2, x)], dict(label=label2, color=None, linestyle="dashed")),
     ]
     return kwargs, plot_calls, mock_figure
 
 
 @parametrize_with_cases(argnames=["kwargs", "plot_calls", "figure"], cases=THIS_MODULE)
-def test_plot_fitting_without_boundaries(kwargs, plot_calls, figure):
+def test_plot_fitting(kwargs, plot_calls, figure):
     plot_fitting(data=FIT_DATA, func=FUNC, title_name=TITLE_NAME, **kwargs)
     ax = figure.add_subplot.return_value
     assert_calls(ax.plot, plot_calls, rel=EPSILON)
