70a437c7accc1dfe1a9929d0f86b86aefc8ac803
Sagi Shadur <saroad2@gmail.com>
2021-02-23 21:16:29 +0200

    Fix isort error

--- src/eddington/__init__.py
+++ src/eddington/__init__.py
@@ -43,7 +43,7 @@ from eddington.plot.plot_legacy import (
     plot_residuals,
     title,
 )
-from eddington.plot.plot_util import show_or_export, build_repr_string
+from eddington.plot.plot_util import build_repr_string, show_or_export
 from eddington.print_util import to_relevant_precision_string
 from eddington.random_util import random_data
 
