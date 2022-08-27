2690c3c34dd05367bb7daed92cd1719dcac4c483
Sagi Shadur <saroad2@gmail.com>
2022-08-27 23:04:19 +0300

    Fix python 3.7 backward compatibility issue

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ version = "0.0.24.dev1"
 install_requires = [
     "click >= 8.1.3",
     "prettytable >= 3.4.0",
-    "scipy >= 1.9.1",
+    'scipy >= 1.9.1; python_version > "3.7"',
+    'scipy >= 1.7.2, <1.8; python_version == "3.7"',
     "numpy >= 1.23.2",
     "openpyxl >= 3.0.10",
     "matplotlib >= 3.5.3",
