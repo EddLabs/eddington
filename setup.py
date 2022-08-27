77d649f418e40efa58a16754eb490341f6b70d03
Sagi Shadur <saroad2@gmail.com>
2022-08-27 23:13:33 +0300

    Drop python 3.7 version support also

--- setup.py
+++ setup.py
@@ -7,10 +7,8 @@ version = "0.0.24.dev1"
 install_requires = [
     "click >= 8.1.3",
     "prettytable >= 3.4.0",
-    'scipy >= 1.9.1; python_version > "3.7"',
-    'scipy >= 1.7.3, <1.8; python_version == "3.7"',
-    'numpy >= 1.23.2; python version > "3.7"',
-    'numpy >= 1.21.6, <1.22; python version == "3.7"',
+    "scipy >= 1.9.1",
+    "numpy >= 1.23.2",
     "openpyxl >= 3.0.10",
     "matplotlib >= 3.5.3",
     "types-mock >= 4.0.15",
